46c456d00f66e50e92ad976f9b98cfe11999c559
Elliot Winkler <elliot.winkler@gmail.com>
2013-08-14 16:03:05 -0600

    Online::Invoice: Test that id and sales_term_id are nil if not present

--- spec/quickeebooks/online/invoice_spec.rb
+++ spec/quickeebooks/online/invoice_spec.rb
@@ -10,4 +10,28 @@ describe "Quickeebooks::Online::Model::Invoice" do
     invoice.line_items.count.should == 1
     invoice.line_items.first.unit_price.should == 225
   end
+
+  it "does not set id if it is not present" do
+    xml = <<EOT
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Invoice xmlns="http://www.intuit.com/sb/cdm/v2" xmlns:qbp="http://www.intuit.com/sb/cdm/qbopayroll/v1" xmlns:qbo="http://www.intuit.com/sb/cdm/qbo">
+  <Header>
+  </Header>
+</Invoice>
+EOT
+    invoice = Quickeebooks::Online::Model::Invoice.from_xml(xml)
+    invoice.id.should eq nil
+  end
+
+  it "does not set header.sales_term_id if it is not present" do
+    xml = <<EOT
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Invoice xmlns="http://www.intuit.com/sb/cdm/v2" xmlns:qbp="http://www.intuit.com/sb/cdm/qbopayroll/v1" xmlns:qbo="http://www.intuit.com/sb/cdm/qbo">
+  <Header>
+  </Header>
+</Invoice>
+EOT
+    invoice = Quickeebooks::Online::Model::Invoice.from_xml(xml)
+    invoice.header.sales_term_id.should eq nil
+  end
 end
