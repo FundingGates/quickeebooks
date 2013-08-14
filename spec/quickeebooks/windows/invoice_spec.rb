961d5ff4e86551ebc9446acb40dd2c7fbbe285bf
Elliot Winkler <elliot.winkler@gmail.com>
2013-08-14 16:03:30 -0600

    Fix test for Windows::Invoice so it's actually using the right class

--- spec/quickeebooks/windows/invoice_spec.rb
+++ spec/quickeebooks/windows/invoice_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe "Quickeebooks::Online::Model::Invoice" do
+describe "Quickeebooks::Windows::Model::Invoice" do
   it "can parse invoice from XML" do
     xml = onlineFixture("invoice.xml")
-    invoice = Quickeebooks::Online::Model::Invoice.from_xml(xml)
+    invoice = Quickeebooks::Windows::Model::Invoice.from_xml(xml)
     invoice.header.balance.should == 0
     invoice.header.sales_term_id.value.should == "3"
     invoice.id.value.should == "13"
