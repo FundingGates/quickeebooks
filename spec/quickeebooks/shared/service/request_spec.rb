a47283045a7b7bae37b0a4e48b7f52966bb904c2
Elliot Winkler <elliot.winkler@gmail.com>
2013-10-11 12:21:50 -0600

    Add tests for Shared::Service::Request

---
+++ spec/quickeebooks/shared/service/request_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Quickeebooks::Request do
+  describe '#method' do
+    it "returns the request method" do
+      delegate_request = double(method: 'get')
+      request = described_class.new(delegate_request)
+      expect(request.method).to eq 'get'
+    end
+  end
+end
