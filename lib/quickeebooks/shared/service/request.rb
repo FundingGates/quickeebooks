d9c5ffe699ebcff620f37c8bd4a4f0de36c36f86
Elliot Winkler <elliot.winkler@gmail.com>
2013-10-14 15:05:37 -0600

    WIP: Move QB::Request to QB::Shared::Service::Request

--- lib/quickeebooks/shared/service/request.rb
+++ lib/quickeebooks/shared/service/request.rb
@@ -1,17 +1,21 @@
 require 'delegate'
 
 module Quickeebooks
-  class Request < SimpleDelegator
-    attr_reader :request
-    attr_accessor :uri
+  module Shared
+    module Service
+      class Request < SimpleDelegator
+        attr_reader :request
+        attr_accessor :uri
 
-    def initialize(request)
-      super(request)
-      @request = request
-    end
+        def initialize(request)
+          super(request)
+          @request = request
+        end
 
-    def method
-      request.method
+        def method
+          request.method
+        end
+      end
     end
   end
 end
--- spec/quickeebooks/shared/service/request_spec.rb
+++ spec/quickeebooks/shared/service/request_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Quickeebooks::Request do
+describe Quickeebooks::Shared::Service::Request do
   describe '#method' do
     it "returns the request method" do
       delegate_request = double(method: 'get')
