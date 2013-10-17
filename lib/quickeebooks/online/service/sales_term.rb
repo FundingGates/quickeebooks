15057fc0f1499e9ab1f0c95e79f562c5ccc7a459
Elliot Winkler <elliot.winkler@gmail.com>
2013-10-17 11:30:49 -0600

    SalesTerm XML errors raise verbose IntuitRequestException

--- lib/quickeebooks/online/service/sales_term.rb
+++ lib/quickeebooks/online/service/sales_term.rb
@@ -10,6 +10,21 @@ module Quickeebooks
           url = "#{url_for_resource(Quickeebooks::Online::Model::SalesTerm::REST_RESOURCE)}/#{id}"
           fetch_object(Quickeebooks::Online::Model::SalesTerm, url, { :idDomain => idDomain })
         end
+
+        private
+
+        def fetch_object(model, url, params = {}, options = {})
+          raise ArgumentError, "missing model to instantiate" if model.nil?
+          response = do_http_get(url, params, {'Content-Type' => 'text/xml'})
+
+          xml = parse_xml(response.body)
+          begin
+            element = xml.at_xpath("//xmlns:#{model::XML_NODE}")
+            model.from_xml(element)
+          rescue Nokogiri::XML::XPath::SyntaxError => ex
+            raise IntuitRequestException.new("Error parsing XML: #{ex.message}\nHTTP Response: (#{response.code}) #{response.body}")
+          end
+        end
       end
     end
   end
--- spec/quickeebooks/online/services/sales_term_spec.rb
+++ spec/quickeebooks/online/services/sales_term_spec.rb
@@ -34,4 +34,15 @@ describe "Quickeebooks::Online::Service::SalesTerm" do
     sales_term.meta_data.last_updated_time.should == Time.parse('2013-01-17T19:04:19-08:00')
     sales_term.name.should == "Net 30"
   end
+
+  context 'when a XML parsing error occurs' do
+    it "raises an IntuitRequestException" do
+      xml = ''
+      url = @service.url_for_resource(Quickeebooks::Online::Model::SalesTerm.resource_for_singular)
+      url = "#{url}/99?idDomain=QB"
+      FakeWeb.register_uri(:get, url, :status => ["200", "OK"], :body => xml)
+      expect{ @service.fetch_by_id(99) }.to raise_error(IntuitRequestException)
+    end
+  end
+
 end
