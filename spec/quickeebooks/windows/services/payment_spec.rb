a99cd4f71cd0dde2291448631002393f4254ce43
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2013-12-30 17:24:19 -0500

    ExternalKey should be typed.

--- spec/quickeebooks/windows/services/payment_spec.rb
+++ spec/quickeebooks/windows/services/payment_spec.rb
@@ -23,7 +23,7 @@ describe "Quickeebooks::Windows::Service::Payment" do
     header = payment.header
     header.customer_name.should == "Davis"
 
-    payment.external_key.should == "4"
+    payment.external_key.value.should == "4"
 
     line1 = payment.line_items.first
     line1.should_not be_nil
