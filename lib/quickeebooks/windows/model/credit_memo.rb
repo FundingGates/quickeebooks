7f05648a6cbe7d78ba807fe566920fbf9af62420
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2013-12-31 15:01:52 -0500

    Adding type for credit memo external key

--- lib/quickeebooks/windows/model/credit_memo.rb
+++ lib/quickeebooks/windows/model/credit_memo.rb
@@ -1,4 +1,5 @@
 require 'quickeebooks/windows/model/id'
+require 'quickeebooks/windows/model/external_key'
 require 'quickeebooks/windows/model/credit_memo_header'
 require 'quickeebooks/windows/model/credit_memo_line_item'
 require 'quickeebooks/windows/model/address'
@@ -21,7 +22,7 @@ module Quickeebooks
         xml_accessor :id, :from => 'Id', :as => Quickeebooks::Windows::Model::Id
         xml_accessor :sync_token, :from => 'SyncToken', :as => Integer
         xml_accessor :meta_data, :from => 'MetaData', :as => Quickeebooks::Windows::Model::MetaData
-        xml_accessor :external_key, :from => 'ExternalKey'
+        xml_accessor :external_key, :from => 'ExternalKey', :as => Quickeebooks::Windows::Model::ExternalKey
         xml_accessor :synchronized, :from => 'Synchronized'
         xml_accessor :custom_fields, :from => 'CustomField', :as => [Quickeebooks::Windows::Model::CustomField]
         xml_accessor :draft
